d453da0adaa5bd0ad8f7470ae37873116c24e4fd
daniflorialopez <daniflorialopez@gmail.com>
2022-07-14 13:04:24 +0200

    Renombrar main

---
+++ TA9 - Herencia en JAVA/src/ejercicio1/Ejercicio1MainApp.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1MainApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Electrodomestico[] electrodomesticos = new Electrodomestico[10];
+		for(int i = 0; i < 10; i++) {
+			
+		}
+
+		
+	}
+
+}
