bd1302eb069db7dac6a51bc06026358a42c4e8e9
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-14 13:10:47 +0200

    Commit entregable

---
+++ TA9 - Herencia en JAVA/src/ejercicio2/Entregable.java
@@ -0,0 +1,10 @@
+package ejercicio2;
+
+public interface Entregable {
+
+    public void entregar();
+    public void devolver();
+    public boolean isEntregado();
+    public void compareTo (Object a);
+
+}
