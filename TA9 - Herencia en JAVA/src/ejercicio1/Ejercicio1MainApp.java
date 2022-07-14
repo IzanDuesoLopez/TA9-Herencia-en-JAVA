74446c1e29ba248d4f902e0b581406355c0e99fb
daniflorialopez <daniflorialopez@gmail.com>
2022-07-14 16:37:28 +0200

    Creación de electrodomésticos y calcular su precio final

--- TA9 - Herencia en JAVA/src/ejercicio1/Ejercicio1MainApp.java
+++ TA9 - Herencia en JAVA/src/ejercicio1/Ejercicio1MainApp.java
@@ -8,11 +8,48 @@ public class Ejercicio1MainApp {
 		Scanner sc = new Scanner(System.in);
 		
 		Electrodomestico[] electrodomesticos = new Electrodomestico[10];
-		for(int i = 0; i < 10; i++) {
-			
-		}
-
+		crear_electrodomesticos(electrodomesticos, sc);
+		calcular_precio_final(electrodomesticos);
 		
 	}
 
+	// Instancia diez objetos y se añaden a la array de electrodomésticos
+	public static void crear_electrodomesticos(Electrodomestico[] electrodomesticos, Scanner sc) {
+		Electrodomestico lavavajillas = new Electrodomestico();
+		Electrodomestico dvd = new Electrodomestico(35.50, 3.50);
+		Electrodomestico microondas = new Electrodomestico(50, "blanco", 'A', 5);
+		Television tv1 = new Television();
+		Television tv2 = new Television(120, 8);
+		Television tv3 = new Television(250, 11);
+		Television tv4 = new Television(500, "negro", 'C', 14, 2048, true);
+		Lavadora lavadora1 = new Lavadora();
+		Lavadora lavadora2 = new Lavadora(199.99, 50);
+		Lavadora lavadora3 = new Lavadora(300, "gris", 'B', 60.25, 50);
+		
+		electrodomesticos[0] = lavavajillas;
+		electrodomesticos[1] = dvd;
+		electrodomesticos[2] = microondas;
+		electrodomesticos[3] = tv1;
+		electrodomesticos[4] = tv2;
+		electrodomesticos[4] = tv3;
+		electrodomesticos[6] = tv4;
+		electrodomesticos[7] = lavadora1;
+		electrodomesticos[8] = lavadora2;
+		electrodomesticos[9] = lavadora3;
+	}
+	
+	// Calcula precio final de todos los electrodomésticos
+	public static void calcular_precio_final(Electrodomestico[] electrodomesticos) {
+		for(Electrodomestico x : electrodomesticos) {
+			x.precioFinal();
+		}
+		
+	}
+	
+	// Calcula precio de cada clase
+	public static void calcular_precio_por_clase(Electrodomestico[] electrodomesticos) {
+		for(Electrodomestico x : electrodomesticos) {
+			
+		}
+	}
 }
