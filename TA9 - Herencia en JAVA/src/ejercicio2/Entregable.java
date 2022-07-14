87a884106b07e168828bc2cc7496310b3825bb3d
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-14 13:21:20 +0200

    Entregable serie y videojuego

--- TA9 - Herencia en JAVA/src/ejercicio2/Entregable.java
+++ TA9 - Herencia en JAVA/src/ejercicio2/Entregable.java
@@ -4,7 +4,7 @@ public interface Entregable {
 
     public void entregar();
     public void devolver();
-    public boolean isEntregado();
+    public boolean estaEntregado();
     public void compareTo (Object a);
 
 }
