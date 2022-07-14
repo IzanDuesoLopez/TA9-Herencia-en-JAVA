77e43d2030311a1351e9a82502fb2efc6f1be61e
IzanDuesoLopez <izandueso@gmail.com>
2022-07-14 12:33:44 +0200

    12:33

--- TA9 - Herencia en JAVA/src/ejercicio1/Lavadora.java
+++ TA9 - Herencia en JAVA/src/ejercicio1/Lavadora.java
@@ -4,17 +4,17 @@ public class Lavadora extends Electrodomestico{
 	
 	private double carga;
 	
-	private static final double cargaDefault = 5.0;
+	private static final double CARGA_DEFAULT = 5.0;
 	
 	// Constructor por defecto
 	public Lavadora () {
-		this.carga = cargaDefault;
+		this.carga = CARGA_DEFAULT;
 	}
 
 	// Constructor con el precio y peso, el resto por defecto
 	public Lavadora(double precioBase, double peso) {
 		super(precioBase, peso);
-		this.carga = cargaDefault;
+		this.carga = CARGA_DEFAULT;
 	}
 
 	// Constructor con la carga y el resto de atributos heredados
