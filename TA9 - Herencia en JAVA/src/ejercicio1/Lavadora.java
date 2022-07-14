0802eadd04f8fb0e110f0a095e62fc6c8834b0d0
IzanDuesoLopez <izandueso@gmail.com>
2022-07-14 11:50:23 +0200

    11:50

---
+++ TA9 - Herencia en JAVA/src/ejercicio1/Lavadora.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+public class Lavadora extends Electrodomestico{
+	
+	private double carga;
+	
+	private static final double cargaDefault = 5.0;
+	
+	public Lavadora () {
+		
+	}
+	
+}
