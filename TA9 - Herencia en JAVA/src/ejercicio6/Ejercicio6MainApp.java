6c21d5bcf2ef8b027f2ff6a5d5449b1a4422726a
IzanDuesoLopez <izandueso@gmail.com>
2022-07-15 17:34:05 +0200

    Clase Ejercicio6MainApp estructurada

--- TA9 - Herencia en JAVA/src/ejercicio6/Ejercicio6MainApp.java
+++ TA9 - Herencia en JAVA/src/ejercicio6/Ejercicio6MainApp.java
@@ -6,43 +6,47 @@ import java.util.Scanner;
 public class Ejercicio6MainApp {
 
 	public static void main(String[] args) {
-
 		final int TAM_FILAS = 8;
 		final int TAM_COLS = 8;
+		String salaCine[][] = new String[TAM_FILAS][TAM_COLS]; // Matriz con las posiciones de la sala
+		boolean estaOcupado[][] = new boolean[TAM_FILAS][TAM_COLS]; // Matriz que nos indica si la posición esta ocupada o no
+		char abecedario[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+		menuCine(salaCine, estaOcupado, abecedario); // Llamamos al método principal del menú
+	}
 
-		String salaCine[][] = new String[TAM_FILAS][TAM_COLS]; 
-		boolean estaOcupado[][] = new boolean[TAM_FILAS][TAM_COLS];
-		char abecedario[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-				'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-
+	// Método del menu cine
+	public static void menuCine(String[][] salaCine, boolean[][] estaOcupado, char[] abecedario) {
+		// Pedimos al espectador que tome asiento
 		System.out.println("Las casillas marcadas con una X estan ocupadas, elige tu sitio a continuación: \n");
-		inicializarSalaCine(salaCine, estaOcupado, abecedario);
-		mostrarSalaCine(salaCine, estaOcupado, abecedario);
-		
+		inicializarSalaCine(salaCine, estaOcupado, abecedario); // Inicializamos la matriz que contiene la información de la sala de cine
+		mostrarSalaCine(salaCine, estaOcupado, abecedario); // Mostramos la sala de cine
+
+		// Seleccionamos asiento
 		System.out.println("Selecciona posicion");
 		Scanner sc = new Scanner(System.in);
 		String asiento = sc.next();
 		
-		boolean noValido = false;
+		// Recorremos la sala del cine
 		for (int fil = 0; fil < salaCine.length; fil++) {
 			for (int col = 0; col < salaCine[fil].length; col++) {
-				if(asiento.equals(salaCine[fil][col]) && estaOcupado[fil][col] == false) {
-					estaOcupado[fil][col] = true;
+				// Si el asiento existe y no esta ocupado
+				if (asiento.equals(salaCine[fil][col]) && estaOcupado[fil][col] == false) {
+					estaOcupado[fil][col] = true; // El asiento se ocupa ya que el espectador se sienta
 				}
 			}
 		}
-
-		mostrarSalaCine(salaCine, estaOcupado, abecedario);
 		
+		mostrarSalaCine(salaCine, estaOcupado, abecedario); // Mostramos la sala de cine
 	}
 
-	// Método que
+	// Método que muestra la sala de cine y sus posiciones
 	public static void mostrarSalaCine(String[][] salaCine, boolean[][] estaOcupado, char[] abecedario) {
+		// Recorremos la matriz mostrando filas y columnas
 		for (int fil = 0; fil < estaOcupado.length; fil++) {
 			for (int col = 0; col < estaOcupado[fil].length; col++) {
-				if (!estaOcupado[fil][col]) {
+				if (!estaOcupado[fil][col]) { // Si no esta ocupada, mostramos el asiento vacio
 					System.out.print("[ ]" + " ");
-				} else {
+				} else { // Si esta ocupada mostramos el asiento ocupado
 					System.out.print("[X]" + " ");
 				}
 			}
@@ -62,16 +66,17 @@ public class Ejercicio6MainApp {
 
 	// Método que inicializa la sala de cine
 	public static void inicializarSalaCine(String[][] salaCine, boolean[][] estaOcupado, char[] abecedario) {
+		// Inicializamos la matriz que nos indica si un asiento esta ocupado o no
 		for (int fil = 0; fil < estaOcupado.length; fil++) {
 			for (int col = 0; col < estaOcupado[fil].length; col++) {
-				estaOcupado[fil][col] = false;
+				estaOcupado[fil][col] = false; // Inicialmente los asientos estaran vacios
 			}
 		}
 
 		// Asignamos los valores a la matriz del cine y mostramos por tamaño la sala
 		for (int fil = 0; fil < salaCine.length; fil++) {
 			for (int col = 0; col < salaCine[fil].length; col++) {
-				salaCine[fil][col] = String.valueOf(fil + 1) + abecedario[col];
+				salaCine[fil][col] = String.valueOf(fil + 1) + abecedario[col]; // Cada posición de la matriz sera número de fila + letra de columna
 			}
 		}
 	}
