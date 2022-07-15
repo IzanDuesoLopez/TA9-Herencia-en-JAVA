915ac9a928527ca59cc960fa37e7e2a101cbf132
IzanDuesoLopez <izandueso@gmail.com>
2022-07-15 11:35:39 +0200

    PeliculaInicio

--- TA9 - Herencia en JAVA/src/ejercicio6/Pelicula.java
+++ TA9 - Herencia en JAVA/src/ejercicio6/Pelicula.java
@@ -1,5 +1,25 @@
 package ejercicio6;
 
-public class Pelicula {
+public class Pelicula extends Cine{
+	
+	private String titulo;
+	private int duracion; // Duracion en minutos
+	private int edadMinima;
+	private String director;
+	
+	public Pelicula() {
+		this.titulo = "";
+		this.duracion = 0;
+		this.edadMinima = 0;
+		this.director = "";
+	}
 
+	public Pelicula(String nombre, double precioEntrada, String titulo, int duracion, int edadMinima, String director) {
+		super(nombre, precioEntrada);
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.edadMinima = edadMinima;
+		this.director = director;
+	}
+	
 }
