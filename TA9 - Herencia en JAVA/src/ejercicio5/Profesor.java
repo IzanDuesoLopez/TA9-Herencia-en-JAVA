7490716251e03c6f11090fada341904a56bcd284
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 10:06:40 +0200

    Commit profesor acabado

--- TA9 - Herencia en JAVA/src/ejercicio5/Profesor.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Profesor.java
@@ -36,7 +36,20 @@ public class Profesor extends Persona {
 		return falta;
 	}
 	
+	// Genero su edad random
+	public int edad() {
+		Random rnd = new Random();
+		return (int) (rnd.nextDouble() * 30 + 20); // Edad de 20 a 50
+	}
+	
+	// Genero su materia random
+		private String generarMateria() {
+			Random rnd = new Random();
+			// Un array donde esten las materias
+			String[] nombreMateria = {"Matematicas", "Filosofia", "Fisica"};
+			// Genero un numero random en funcion del array
+			return nombreMateria[(int) (rnd.nextDouble() * nombreMateria.length)];
+		}
 	
 	
-
 }
