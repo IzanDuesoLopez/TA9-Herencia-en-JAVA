5709dca70a8f820cef22d767de120518819fbe1d
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 11:41:52 +0200

    Commit espectador acabado (en teoria)

--- TA9 - Herencia en JAVA/src/ejercicio6/Espectador.java
+++ TA9 - Herencia en JAVA/src/ejercicio6/Espectador.java
@@ -1,5 +1,64 @@
 package ejercicio6;
 
 public class Espectador {
+	
+	// Atributos
+	private String nombre;
+	private int edad;
+	private double dinero;
+	
+	// Constructores
+	
+	public Espectador () {
+		this("anonimo", 18, 0);
+	}
+	
+	public Espectador (String nombre, int edad, double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public double getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(double dinero) {
+		this.dinero = dinero;
+	}
+	
+	// Métodos
+	public boolean aptoEdad(Pelicula peli) {
+		boolean apto = false;
+		
+		if (getEdad() > peli.getEdadMinima()) {
+			apto = true;
+		}
+		
+		return apto;
+	}
+	
+	public boolean compraEntrada (Cine cine) {
+		if (cine.getprecioEntrada() > getDinero()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
