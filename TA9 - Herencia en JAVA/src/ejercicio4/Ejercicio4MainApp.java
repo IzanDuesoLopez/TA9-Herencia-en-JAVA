3f4bd23539e48778563cdbfd4d8718027a36302a
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 11:10:54 +0200

    Commit 4 main app

--- TA9 - Herencia en JAVA/src/ejercicio4/Ejercicio4MainApp.java
+++ TA9 - Herencia en JAVA/src/ejercicio4/Ejercicio4MainApp.java
@@ -5,9 +5,24 @@ public class Ejercicio4MainApp {
 	public static void main(String[] args) {
 		
 
-		Raices raiz = new Raices (3, -10, 1);
+		Raices raiz = new Raices (9, -8, 1);
+		raiz.getDiscriminante();
 		
-		raiz.calcular();
+		Raices raiz1 = new Raices (10, -4, 2);
+		raiz1.tieneRaices();
+		
+		Raices raiz2 = new Raices (7, 5, 4);
+		raiz2.tieneRaiz();
+		
+		Raices raiz3 = new Raices (8, -2, 3);
+		raiz3.calcular();
+		
+		Raices raiz4 = new Raices (7, 13, 3);
+		raiz4.obtenerRaices();
+		
+		Raices raiz5 = new Raices (13, -22, 10);
+		raiz5.obtenerRaiz();
+		
 	}
 
 }
