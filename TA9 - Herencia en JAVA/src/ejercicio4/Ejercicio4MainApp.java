71f4764a6d2c107dd27c79ed65718a374096afc5
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 11:02:21 +0200

    Commit ejercicio 4 main app

---
+++ TA9 - Herencia en JAVA/src/ejercicio4/Ejercicio4MainApp.java
@@ -0,0 +1,13 @@
+package ejercicio4;
+
+public class Ejercicio4MainApp {
+	
+	public static void main(String[] args) {
+		
+
+		Raices raiz = new Raices (3, -10, 1);
+		
+		raiz.calcular();
+	}
+
+}
