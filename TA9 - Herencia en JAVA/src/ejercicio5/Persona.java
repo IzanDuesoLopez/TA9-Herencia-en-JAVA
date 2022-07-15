edf19ac8ae63e25bb3bee91dfebd8276a0313417
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 10:44:15 +0200

    Commit persona + edad()

--- TA9 - Herencia en JAVA/src/ejercicio5/Persona.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Persona.java
@@ -52,6 +52,9 @@ public abstract class Persona {
 	// Método abstracto que heredaran las clases hijas para comprobar si estan en clase
 	public abstract boolean noEstaEnClase();
 	
+	// Método abstracto para generar edad
+	public abstract int edad();
+	
 	// Método abstracto que heredaran las clases hijas para comprobar si estan en clase
 	public abstract char esHombreOMujer();
 	
