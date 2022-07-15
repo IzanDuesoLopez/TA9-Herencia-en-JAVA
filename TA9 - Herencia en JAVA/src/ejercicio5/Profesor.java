06103ed174c55efe43b5ce3677ecb689179842a6
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 09:57:47 +0200

    Commit profesor empezado

--- TA9 - Herencia en JAVA/src/ejercicio5/Profesor.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Profesor.java
@@ -1,5 +1,42 @@
 package ejercicio5;
 
-public class Profesor {
+import java.util.Random;
+
+public class Profesor extends Persona {
+	
+	// Atributos
+	private String materia;
+
+	public String getMateria() {
+		return materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+	
+	// Constructores
+	
+	public Profesor () {
+		this.edad = edad();
+		this.materia = generarMateria();
+		this.falta = ausencia();
+	}
+	
+	// Metodos
+	public boolean ausencia() {
+		Random rdm = new Random();
+		boolean falta = false;
+		
+		// Generamos un numero random
+		if ((int) (rdm.nextDouble() * 10 + 1) > 8) {
+			falta = true;
+		}
+		
+		return falta;
+	}
+	
+	
+	
 
 }
