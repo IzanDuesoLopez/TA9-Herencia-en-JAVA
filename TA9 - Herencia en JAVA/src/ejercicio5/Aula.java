da14ea3a4576bdd73596ecd2ce3ee0455385934b
daniflorialopez <daniflorialopez@gmail.com>
2022-07-15 20:41:32 +0200

    Métodos para autogenerar elementos

--- TA9 - Herencia en JAVA/src/ejercicio5/Aula.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Aula.java
@@ -11,14 +11,30 @@ public class Aula {
 	private ArrayList<Alumno> alumnos;
 	private boolean se_puede_dar_clase;
 	
-	// Constructor
+	// Constructores
+	public Aula() {
+		this.identificador = generar_identificador();
+		this.maximo_estudiantes = 50;
+		this.materia = elegir_materia();
+		this.profesor = new Profesor();
+		this.alumnos = new ArrayList<Alumno>();
+		generar_alumnos();
+		this.se_puede_dar_clase = darClase(alumnos, profesor, materia, maximo_estudiantes);
+		if(this.se_puede_dar_clase) {
+			mostrarNumeroAprobados(alumnos);
+		}
+	}
+	
 	public Aula(int identificador, int maximo_estudiantes, String materia, Profesor profesor, ArrayList<Alumno> alumnos) {
 		this.identificador = identificador;
 		this.maximo_estudiantes = maximo_estudiantes;
 		this.materia = materia;
 		this.profesor = profesor;
 		this.alumnos = alumnos;
-		this.se_puede_dar_clase = darClase(alumnos, profesor, materia);
+		this.se_puede_dar_clase = darClase(alumnos, profesor, materia, maximo_estudiantes);
+		if(this.se_puede_dar_clase) {
+			mostrarNumeroAprobados(alumnos);
+		}
 	}
 	
 	// Getters y setters
@@ -62,27 +78,68 @@ public class Aula {
 	}
 	
 	// Métodos 
-	public static boolean darClase(ArrayList<Alumno> alumnos, Profesor profesor, String materia) { //
+	public static boolean darClase(ArrayList<Alumno> alumnos, Profesor profesor, String materia, int maximo_estudiantes) { //
 		int alumnos_presentes = 0;
 		for(Alumno alumno : alumnos) {
 			if(alumno != null) 
 				alumnos_presentes++;
 		}
-		if((profesor != null && profesor.getMateria().equals(materia) && alumnos_presentes >= alumnos.size() / 2))
-			return true;
+		if(!(profesor != null)) {
+			System.out.println("No se puede dar clase. No hay profesor.");
+			return false;
+		} else if(!(profesor.getMateria().equals(materia))) {
+			System.out.println("No se puede dar clase. El profesor no da la materia correspondiente.");
+			return false;
+		} else if(alumnos_presentes < alumnos.size() / 2) {
+			System.out.println("No se puede dar clase. Hay " + alumnos.size() + " alumnos de los " + maximo_estudiantes + " máximos.");
+			return false;
+		}
 		
-		return false;
+		System.out.println("Se puede dar clase. Hay " + alumnos.size() + " alumnos de los " + maximo_estudiantes + " máximos.");
+		return true;
 	}
 	
 	public static void mostrarNumeroAprobados(ArrayList<Alumno> alumnos) {
 		int numero_alumnos = 0;
 		int numero_alumnas = 0;
 		for(Alumno alumno : alumnos) {
-			if(alumno.getSexo() == 'H' && alumno.getCalificacion() >= 5) numero_alumnos++;
-			else if(alumno.getSexo() == 'M' && alumno.getCalificacion() >= 5) numero_alumnas++;
+			if(alumno.getSexo() == 'H' && alumno.getCalificacion() >= 5) 
+				numero_alumnos++;
+			else if(alumno.getSexo() == 'M' && alumno.getCalificacion() >= 5) 
+				numero_alumnas++;
 		}
 		System.out.println("Alumnos aprobados: " + numero_alumnos);
 		System.out.println("Alumnas aprobadas: " + numero_alumnas);
 		
+	}
+	
+	public static int generar_identificador() {
+		int longitud = 4;
+		int posicion_caracter;
+		String nuevo_identificador = "";
+		
+		for(int i = 0; i < longitud; i++) {
+			posicion_caracter = (int)(Math.random() * 9);
+			nuevo_identificador += posicion_caracter;
+		}
+		
+		return Integer.parseInt(nuevo_identificador);
+	}
+	
+	public static String elegir_materia() {
+		String[] materias = {"matemáticas", "filosofía", "física"};
+		//(int)(Math.random() * (max - min)) + min;
+		int posicion = (int)(Math.random() * (materias.length - 1));
+		return materias[posicion];
+		
+	}
+	
+	public void generar_alumnos() {
+		//(int)(Math.random() * (max - min)) + min;
+		int numero_estudiantes = (int)(Math.random() * this.maximo_estudiantes);
+		for(int i = 0; i < numero_estudiantes; i++) {
+			Alumno alumno = new Alumno();
+			this.alumnos.add(alumno);
+		}
 	}
 }
--- TA9 - Herencia en JAVA/src/ejercicio5/Ejercicio5MainApp.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Ejercicio5MainApp.java
@@ -3,8 +3,7 @@ package ejercicio5;
 public class Ejercicio5MainApp {
 
 	public static void main(String[] args) {
-		
-
+		Aula aula = new Aula();
+		System.out.println(aula.getAlumnos().toString());
 	}
-
 }
