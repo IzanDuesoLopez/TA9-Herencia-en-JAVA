832aa0dcef982b0527dae3560424c5e84fc5ff3d
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-15 09:41:08 +0200

    Commit atributos y setters

--- TA9 - Herencia en JAVA/src/ejercicio5/Persona.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Persona.java
@@ -1,5 +1,37 @@
 package ejercicio5;
 
 public abstract class Persona {
+	
+	// Atributos
+	protected String nombre;
+	protected int edad;
+	protected char sexo;
+	protected boolean falta;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public boolean isFalta() {
+		return falta;
+	}
+	public void setFalta(boolean falta) {
+		this.falta = falta;
+	}
+	
+	
 
 }
