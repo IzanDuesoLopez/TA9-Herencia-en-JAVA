13fd428fd81fe231d629e3312d16bd4f583ffa95
IzanDuesoLopez <izandueso@gmail.com>
2022-07-15 09:40:22 +0200

    noEstaEnClase();

--- TA9 - Herencia en JAVA/src/ejercicio5/Persona.java
+++ TA9 - Herencia en JAVA/src/ejercicio5/Persona.java
@@ -1,5 +1,8 @@
 package ejercicio5;
 
 public abstract class Persona {
-
+	
+	
+	// Método abstracto que heredaran las clases hijas para comprobar si estan en clase
+	public abstract boolean noEstaEnClase();
 }
